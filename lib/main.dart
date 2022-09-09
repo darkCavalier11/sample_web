8b62b123101ab7bc6f170b7bcf1b956f40498890
darCavalier11 <sumit.pradhan65@gmail.com>
2022-09-09 13:09:09 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Dalei harami',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
+        ),
+      ),
+    );
+  }
+}
